3ae9011b7f163c8cd54f0de4ae0e2c209f5c04f8
Batiebo <gabi47507@gmail.com>
2023-08-04 11:54:57 +0000

    premier commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnForward = document.querySelector('button');
+const indicator = document.querySelector('.indicator .line span');
+const indicatorItems = document.querySelector('.indicator p');
+const form = document.querySelector('form');
+const allForms = document.querySelectorAll('form');
